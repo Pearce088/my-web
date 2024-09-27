12180f65d98c8bdb96d15f57b26dd8c0d1f18dd8
Pearce088 <newbiewff098@gmail.com>
2024-09-27 12:39:52 +0700

    deleted:    public/favicon.ico 	new file:   public/jt-logo.png 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx 	modified:   src/styles/globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 export default function page () {
     return (
-        <div>
-            <h1>HAIIIIIII TESSS</h1>
+        <div className="nav-header">
+            <nav className="d-flex">
+                <div className="px-5 marketplace-name">
+                    <img src="jt-logo.png" alt="jt-logo" />
+                </div>
+                <div className="ms-auto my-auto">
+                    <ul className="menu-buttons px-5">
+                        <li><a href="#myCarousel" className="text-decoration-none text-dark fw-bold">Home</a></li>
+                        <li><a href="#product" className="text-decoration-none text-dark fw-bold">Product</a></li>
+                        <li><a href="#order" className="text-decoration-none text-dark fw-bold">How to Order</a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 }
